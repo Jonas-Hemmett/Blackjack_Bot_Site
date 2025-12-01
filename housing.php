2aa1d0986e771d30fcf1abb26dddbd2c5f2c2308
JonasH <johannes.hemmett@uvm.edu>
2025-12-01 16:03:44 -0500

    23

--- housing.php
+++ housing.php
@@ -7,7 +7,7 @@
         <section>
             <h2>Custom Housing</h2>
             <p>
-               TIn order to combine features like a camera, screen, and buttons with  the Raspberry Pi, I made a custom housing that fits all of them. I used CAD (Computer aided design) to design it on my computer.  I created sketches of the parts, combining dimensions of the Pi with the ideas I had in my head. Designing the custom housing took many iterations to get right as I had to make sure all of the parts fit together, many of the intermediate versions were made of paper while I worked with the FabLab to laser cut the final design out of acrylic. My favorite feature is the angle gauge on the side of the camera mount. Other features include engraved text, cable management for the camera, recessing the camera and buttons for increased durability, and covering the exposed circuitry.
+               In order to combine features like a camera, screen, and buttons with  the Raspberry Pi, I made a custom housing that fits all of them. I used CAD (Computer aided design) to design it on my computer.  I created sketches of the parts, combining dimensions of the Pi with the ideas I had in my head. Designing the custom housing took many iterations to get right as I had to make sure all of the parts fit together, many of the intermediate versions were made of paper while I worked with the FabLab to laser cut the final design out of acrylic. My favorite feature is the angle gauge on the side of the camera mount. Other features include engraved text, cable management for the camera, recessing the camera and buttons for increased durability, and covering the exposed circuitry.
             </p>
         </section>  
         <section>
