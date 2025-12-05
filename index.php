4d8cfdd3a6f50a9f8acb212a711018cccc144a89
JonasH <johannes.hemmett@uvm.edu>
2025-12-05 10:08:51 -0500

    Added Coding Info

--- index.php
+++ index.php
@@ -21,7 +21,9 @@
                 <p>
                     I’m currently a Computer Science major at the University of Vermont. I love creative problem solving and exploring how technology can bring my ideas to life. I enjoy building projects that combine code, CAD, and hands-on experimentation to create practical and creative solutions.<br>
                     <br>
-                    Outside of school, I always try to stay busy. I like skiing, leatherworking & fashion design, and playing games, whether they be video, card, or board.
+                    Outside of school, I always try to stay busy. I like skiing, leatherworking & fashion design, and playing games, whether they be video, card, or board.<br>
+                    <br>
+                    Feel free to check out my personal site at <a href = "https://jhemmett.com/">JHemmett.com</a>.
                 </p>
             </section>  
         </div>
@@ -30,7 +32,7 @@
                 <h2>GitHub</h2>
                 <p>
                     The files for this project are available on my GitHub:
-                         <a href="https://github.com/Jonas-Hemmett/Blackjack_Bot">github.com/Jonas-Hemmett/Blackjack_Bot</a>
+                         <a href="https://github.com/Jonas-Hemmett/Blackjack_Bot">github.com/Jonas-Hemmett/Blackjack_Bot</a>.
                 </p>
                 <!-- Empty sections for padding -->
             </section>  
